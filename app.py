662c4b449545ec53a2b45dc8bfedd613bcaf546b
unknown <akashinfinite42@gmail.com>
2024-04-11 01:09:20 -0400

    added documentation

--- app.py
+++ app.py
@@ -3,22 +3,27 @@ from flask import Flask, redirect, render_template, request
 
 app = Flask(__name__)
 
+#the app.py function to render index page
 @app.get('/')
 def index():
     return render_template('index.html')
 
-
+#renders events page and lists the current database
 @app.get('/events')
 def list_all_events():
-    # TODO: Feature 1
+    # TODO: Complete Feature 1
+    list_all_events = request.form.get('events')
+    Name = request.form.get('name')
+    EventName = request.form.get('director')
+    Time = request.form.get('time')
     return render_template('list_all_events.html', list_events_active=True, event_list = list_all_events.get_all_events())
 
-
+#calls the create events page for the main method of user input
 @app.get('/events/new')
 def create_events_form():
     return render_template('create_events_form.html')
 
-
+#create movies implements the format for the create movies function
 @app.post('/movies')
 def create_movie():
     #basic input for events
@@ -28,11 +33,12 @@ def create_movie():
     #temp address input
     Address = request.form.get('address')
     
+    #to be changed in next ver
     movie_repository.create_movie(Name, EventName, Time, Address)
     return redirect('/movies')
 
 
-
+#event search
 @app.get('/events/search')
 def search_event():
     #todo
